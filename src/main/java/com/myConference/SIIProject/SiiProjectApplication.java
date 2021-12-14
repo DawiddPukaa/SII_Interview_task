d71621ce1d4e1c9c777d24716dec352168060c64
Dawid_Puka <dawidpuka@gmail.com>
2021-12-14 16:42:21 +0100

    init commit

---
+++ src/main/java/com/myConference/SIIProject/SiiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myConference.SIIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myConference/SIIProject/SiiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myConference.SIIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
